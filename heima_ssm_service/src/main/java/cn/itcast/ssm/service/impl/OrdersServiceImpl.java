1cc3c99fa4881d8c14c4a9e960ca4e039d8ca59b
lxj <469715603@qq.com>
2019-03-09 11:46:09 +0800

    删除一些无用的注解

--- heima_ssm_service/src/main/java/cn/itcast/ssm/service/impl/OrdersServiceImpl.java
+++ heima_ssm_service/src/main/java/cn/itcast/ssm/service/impl/OrdersServiceImpl.java
@@ -11,7 +11,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
 public class OrdersServiceImpl implements OrdersService {
     @Autowired
     private OrdersDao ordersDao;
--- heima_ssm_service/src/main/java/cn/itcast/ssm/service/impl/PermissionServletImpl.java
+++ heima_ssm_service/src/main/java/cn/itcast/ssm/service/impl/PermissionServletImpl.java
@@ -12,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 @Service
-@Transactional
 public class PermissionServletImpl implements PermissionService {
     @Autowired
     private PermissionDao permissionDao;
